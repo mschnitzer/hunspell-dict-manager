f8803027dbd70722275d8c8717e5b55a304639bd
Thomas Schraitle <tom_schr@web.de>
2015-03-26 09:05:33 +0100

    Improved code and style
    * Moved checks of args.o and args.f into arg_parser
    * Corrected indendation

--- hunspell-dict-manager.py
+++ hunspell-dict-manager.py
@@ -16,56 +16,56 @@ import os
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", help="option (add, del or build)")
+    parser.add_argument("-o",
+        choices=('add', 'del', 'build'),
+        help="option (add, del or build)")
     parser.add_argument("-w", help="word")
     parser.add_argument("-f", help="file")
     parser.add_argument("-d", help="dictionary output file")
-    content = parser.parse_args()
+    args = parser.parse_args()
 
-    return content
+    if args.w and args.w[0] == "'":
+        # Remove shell quoting
+        args.w = args.w[1:-1]
+
+    if args.o in ('add', 'del') and not args.f:
+        parser.error("Missing filename")
+
+    return args
 
 def spell_add(args):
-    """Adding words to wordlist
     """
-    if args.w and args.f:
-        if args.w[0] == "'":
-            args.w = args.w[1:-1]
-            os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && echo \"" + args.w + "\" >> /tmp/wordlistmgr.tmp && rm " + args.f + " && touch " + args.f + " && sort /tmp/wordlistmgr.tmp | uniq >> " + args.f + " && rm /tmp/wordlistmgr.tmp")
-            print("Word added: " + args.w)
-        else:
-            print("Invalid arguments")
+    Adding words to wordlist
+    """
+    print(args)
+    os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && echo \"" + args.w + "\" >> /tmp/wordlistmgr.tmp && rm " + args.f + " && touch " + args.f + " && sort /tmp/wordlistmgr.tmp | uniq >> " + args.f + " && rm /tmp/wordlistmgr.tmp")
+    print("Word added: " + args.w)
 
 def spell_del(args):
-    """Deleting word from wordlist
     """
-    if args.w and args.f:
-        os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && cat /tmp/wordlistmgr.tmp | grep -v ^" + args.w + "$ > " + args.f + " && rm /tmp/wordlistmgr.tmp")
-        print("Deleted word: " + args.w)
-    else:
-        print("Invalid arguments")
-
+    Deleting word from wordlist
+    """
+    os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && cat /tmp/wordlistmgr.tmp | grep -v ^" + args.w + "$ > " + args.f + " && rm /tmp/wordlistmgr.tmp")
+    print("Deleted word: " + args.w)
 
 def spell_build(args):
-    """Build dictionary
+    """
+    Build dictionary
     """
     if os.geteuid() != 0:
         print("You have to be root to build the dictionary because you can't touch the /usr/share/myspell directory as user.")
     else:
-        if args.d and args.f:
-            os.system("wc -l " + args.f + " > /usr/share/myspell/" + args.d + ".dic && sort " + args.f + " | uniq >> /usr/share/myspell/" + args.d + ".dic && touch /usr/share/myspell/" + args.d + ".aff")
-            print("Generated dictionary")
-        else:
-            print("Invalid arguments")
+        os.system("wc -l " + args.f + " > /usr/share/myspell/" + args.d + ".dic && sort " + args.f + " | uniq >> /usr/share/myspell/" + args.d + ".dic && touch /usr/share/myspell/" + args.d + ".aff")
+        print("Generated dictionary")
 
 if __name__ == '__main__':
     args = arg_parser()
 
-    funcmap = { 'add':   spell_add,
-          'del':   spell_del,
-          'build': spell_build,
-        }
+    funcmap = {
+        'add':   spell_add,
+        'del':   spell_del,
+        'build': spell_build,
+    }
 
     func = funcmap.get(args.o)
     res = func(args)
-
-# EOF
