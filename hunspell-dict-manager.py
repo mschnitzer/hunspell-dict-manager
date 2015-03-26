5d1f2c352732623342f337ac30400cb2529f5acf
Thomas Schraitle <tom_schr@web.de>
2015-03-26 08:38:45 +0100

    Use dict as mapping from option to function

--- hunspell-dict-manager.py
+++ hunspell-dict-manager.py
@@ -21,32 +21,51 @@ def arg_parser():
     parser.add_argument("-f", help="file")
     parser.add_argument("-d", help="dictionary output file")
     content = parser.parse_args()
-    
+
     return content
 
-if __name__ == '__main__':
-    args = arg_parser()
-    if args.o == "add":
-        if args.w and args.f:
-            if args.w[0] == "'":
-                args.w = args.w[1:-1]
-            
+def spell_add(args):
+    """Adding words to wordlist
+    """
+    if args.w and args.f:
+        if args.w[0] == "'":
+            args.w = args.w[1:-1]
             os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && echo \"" + args.w + "\" >> /tmp/wordlistmgr.tmp && rm " + args.f + " && touch " + args.f + " && sort /tmp/wordlistmgr.tmp | uniq >> " + args.f + " && rm /tmp/wordlistmgr.tmp")
             print("Word added: " + args.w)
         else:
             print("Invalid arguments")
-    elif args.o == "del":
-        if args.w and args.f:
-            os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && cat /tmp/wordlistmgr.tmp | grep -v ^" + args.w + "$ > " + args.f + " && rm /tmp/wordlistmgr.tmp")
-            print("Deleted word: " + args.w)
+
+def spell_del(args):
+    """Deleting word from wordlist
+    """
+    if args.w and args.f:
+        os.system("cat " + args.f + " > /tmp/wordlistmgr.tmp && cat /tmp/wordlistmgr.tmp | grep -v ^" + args.w + "$ > " + args.f + " && rm /tmp/wordlistmgr.tmp")
+        print("Deleted word: " + args.w)
+    else:
+        print("Invalid arguments")
+
+
+def spell_build(args):
+    """Build dictionary
+    """
+    if os.geteuid() != 0:
+        print("You have to be root to build the dictionary because you can't touch the /usr/share/myspell directory as user.")
+    else:
+        if args.d and args.f:
+            os.system("wc -l " + args.f + " > /usr/share/myspell/" + args.d + ".dic && sort " + args.f + " | uniq >> /usr/share/myspell/" + args.d + ".dic && touch /usr/share/myspell/" + args.d + ".aff")
+            print("Generated dictionary")
         else:
             print("Invalid arguments")
-    elif args.o == "build":
-        if os.geteuid() != 0:
-            print("You have to be root to build the dictionary because you can't touch the /usr/share/myspell directory as user.")
-        else:
-            if args.d and args.f:
-                os.system("wc -l " + args.f + " > /usr/share/myspell/" + args.d + ".dic && sort " + args.f + " | uniq >> /usr/share/myspell/" + args.d + ".dic && touch /usr/share/myspell/" + args.d + ".aff")
-                print("Generated dictionary")
-            else:
-                print("Invalid arguments")
+
+if __name__ == '__main__':
+    args = arg_parser()
+
+    funcmap = { 'add':   spell_add,
+          'del':   spell_del,
+          'build': spell_build,
+        }
+
+    func = funcmap.get(args.o)
+    res = func(args)
+
+# EOF
